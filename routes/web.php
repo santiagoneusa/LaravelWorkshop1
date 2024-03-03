c82cce8908ae567c7dc1c3d926076ddfe1e3b849
santiagoneusa <sneusar@gmail.com>
2024-03-03 13:33:39 -0500

    urls for the cart controller created

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::get('/', 'App\Http\Controllers\HomeController@index')->name('home.index');
 
 Route::post('/order/delete/{id}', 'App\Http\Controllers\OrderController@delete')->name('order.delete');
@@ -20,3 +9,7 @@ Route::get('/order', 'App\Http\Controllers\OrderController@index')->name('order.
 Route::get('/order/create', 'App\Http\Controllers\OrderController@create')->name('order.create');
 Route::post('/order/save', 'App\Http\Controllers\OrderController@save')->name('order.save');
 Route::get('/order/{id}', 'App\Http\Controllers\OrderController@show')->name('order.show');
+
+Route::get('/cart', 'App\Http\Controllers\CartController@index')->name('cart.index');
+Route::get('/cart/add/{id}', 'App\Http\Controllers\CartController@add')->name('cart.add');
+Route::get('/cart/removeAll/', 'App\Http\Controllers\CartController@removeAll')->name('cart.removeAll');
